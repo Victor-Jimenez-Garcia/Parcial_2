32d20531f506c91f90dac1d423aac6240c54ba73
Victor Jimenez <victor.jimenez1@udea.edu.co>
2020-12-12 10:51:12 -0500

    Se inicializan las variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-
+#include <math.h>
+#include <vector>
+#define G 9.81
+#define pi 3.141617
 using namespace std;
+
+struct Do{
+    float d = 800;
+    float Ho = 100;
+    float Xo = 0;
+    float Yo = Ho;
+    float d0 = 0.05*d;
+};
+
+struct Dd{
+    float d = 800;
+    float Hd = 20;
+    float Xd = d;
+    float Yd = Hd;
+    float d0 = 0.025*d;
+};
 
 int main()
 {
